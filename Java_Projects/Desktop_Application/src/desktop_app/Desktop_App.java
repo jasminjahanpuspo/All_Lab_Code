949729fc2dfbcdbef296d29381b2666dd9736a99
Jasmin Jahan Puspo <puspo.neub97@gmail.com>
2021-10-30 03:01:03 +0600

    Add files via upload

---
+++ Java_Projects/Desktop_Application/src/desktop_app/Desktop_App.java
@@ -0,0 +1,13 @@
+////////// OS_Lab_Task 5 ///////////////
+////////// 18.02.2020    //////////////
+package desktop_app;
+
+
+public class Desktop_App {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Desktop_Application().setVisible(true);
+    }
+    
+}
